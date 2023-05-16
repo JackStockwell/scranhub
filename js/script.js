ae1861d09f584be865db0f0bb45f76f240b4d6b0
Jack Stockwell <95311730+JackStockwell@users.noreply.github.com>
2023-05-16 19:31:37 +0100

    now displays location result in console

--- js/script.js
+++ js/script.js
@@ -19,19 +19,26 @@ function initMap() {
 
 var locationElement = document.querySelector('#location')
 
-function locationSearch(event) {
+function newSearch (event) {
     event.preventDefault()
-    if (locationElement.value === "") {
+    locationSearch(locationElement.value)
+}
+
+function locationSearch(value) {
+
+    if (value === "") {
         var invalidPara = document.querySelector('#error-msg')
         invalidPara.innerHTML = "Please enter a location!"
         setTimeout(() => {
             invalidPara.innerHTML = ""
         }, 3000)
+    } else {
+        locationFinder(value)
     }
 }
 
 function locationFinder(location) {
-    var apiURL = `https://maps.googleapis.com/maps/api/geocode/json?&address=4+eastern+boulevard+southend&key=${keyAPI}`
+    var apiURL = `https://maps.googleapis.com/maps/api/geocode/json?&address=${location}&key=${keyAPI}`
     console.log(apiURL)
     fetch(apiURL)
         .then(response => response.json())
