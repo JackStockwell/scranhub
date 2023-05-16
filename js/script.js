5764f994741dd5e08b83e1d31e1b1d3c0f32ac24
Jack Stockwell <95311730+JackStockwell@users.noreply.github.com>
2023-05-16 19:25:47 +0100

    begun writing geolocation function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+var keyAPI = "AIzaSyD0LvDbL349Bf8FNEras65YzLL6M_p-Cgc"
 
-
+// Map creation
 let map;
-// initMap is now async
+
+// Map initialiser
 function initMap() {
 
     var options = {
@@ -11,9 +13,32 @@ function initMap() {
             lng: -0.1276,
         }
     }
-
     map = new google.maps.Map(document.getElementById("map"), options)
 
 };
 
-window.initMap = initMap;
+var locationElement = document.querySelector('#location')
+
+function locationSearch(event) {
+    event.preventDefault()
+    if (locationElement.value === "") {
+        var invalidPara = document.querySelector('#error-msg')
+        invalidPara.innerHTML = "Please enter a location!"
+        setTimeout(() => {
+            invalidPara.innerHTML = ""
+        }, 3000)
+    }
+}
+
+function locationFinder(location) {
+    var apiURL = `https://maps.googleapis.com/maps/api/geocode/json?&address=4+eastern+boulevard+southend&key=${keyAPI}`
+    console.log(apiURL)
+    fetch(apiURL)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+
+}
+
+window.initMap = initMap;``
