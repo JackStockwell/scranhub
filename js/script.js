1d56df498246f41e7f704c8a90039306b145c558
dirie93 <mdirie19@gmail.com>
2023-05-16 21:28:11 +0100

    made changes

--- js/script.js
+++ js/script.js
@@ -1,51 +1,46 @@
-var keyAPI = "AIzaSyD0LvDbL349Bf8FNEras65YzLL6M_p-Cgc"
-
 // Map creation
 let map;
 
 // Map initialiser
 function initMap() {
+  var options = {
+    zoom: 8,
+    center: {
+      lat: 51.5072,
+      lng: -0.1276,
+    },
+  };
+  map = new google.maps.Map(document.getElementById("map"), options);
+}
 
-    var options = {
-        zoom: 8,
-        center: {
-            lat: 51.5072,
-            lng: -0.1276,
-        }
-    }
-    map = new google.maps.Map(document.getElementById("map"), options)
+var locationElement = document.querySelector("#location");
 
-};
-
-var locationElement = document.querySelector('#location')
-
-function newSearch (event) {
-    event.preventDefault()
-    locationSearch(locationElement.value)
+function newSearch(event) {
+  event.preventDefault();
+  locationSearch(locationElement.value);
 }
 
 function locationSearch(value) {
-
-    if (value === "") {
-        var invalidPara = document.querySelector('#error-msg')
-        invalidPara.innerHTML = "Please enter a location!"
-        setTimeout(() => {
-            invalidPara.innerHTML = ""
-        }, 3000)
-    } else {
-        locationFinder(value)
-    }
+  if (value === "") {
+    var invalidPara = document.querySelector("#error-msg");
+    invalidPara.innerHTML = "Please enter a location!";
+    setTimeout(() => {
+      invalidPara.innerHTML = "";
+    }, 3000);
+  } else {
+    locationFinder(value);
+  }
 }
 
 function locationFinder(location) {
-    var apiURL = `https://maps.googleapis.com/maps/api/geocode/json?&address=${location}&key=${keyAPI}`
-    console.log(apiURL)
-    fetch(apiURL)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-        })
-
+  var apiURL = `https://maps.googleapis.com/maps/api/geocode/json?&address=${location}&key=${keyAPI}`;
+  console.log(apiURL);
+  fetch(apiURL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
 }
 
-window.initMap = initMap;``
+window.initMap = initMap;
+``;
