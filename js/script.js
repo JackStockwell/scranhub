269c7d2a91097bf951866adb12aaa63be0d16f8b
Jack Stockwell <95311730+JackStockwell@users.noreply.github.com>
2023-05-24 00:09:44 +0100

    recent search code now intergrated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Map creation
 let map;
 let arrayID = []
+let recentResults = []
 
 // Map initialiser
 function initMap() {
@@ -14,35 +15,44 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), options);
 }
 
+// Places the marker on the map for all to see!
+
 function locationMarker(placeObj) {
 
+  // Gets the relevant infomation from the object parsed to create a map.
   const myLatLng = placeObj.geometry.location;
   let address = placeObj.formatted_address;
   let name = placeObj.name
 
-  // Used when a standard placeObj isn't parsed due to Geolocation.
+  // Used when the standard placeObj isn't parsed, geolocation does return a name.
   if (!name) {
     name = "Current location"
   }
 
+  // Makes a new marker.
   const marker = new google.maps.Marker({
     position: myLatLng,
     title: placeObj.name,
   });
 
+  // Populates the marker with content
   const contentInfo = `
     <div id="content">
         <h3>${name}</h3>
         <p>${address}</p>
     </div>
     `;
+
+  // Populates the infowindow with the above content.
   const infoWindow = new google.maps.InfoWindow({
     content: contentInfo,
     ariaLabel: name,
   });
 
+  // Designates the marker to the map
   marker.setMap(map);
 
+  // Event listener to display infowindow.
   marker.addListener("click", () => {
     infoWindow.open({
       anchor: marker,
@@ -51,31 +61,41 @@ function locationMarker(placeObj) {
   });
 }
 
+// Searches for the location given a string and returns a lat and lng of the best matched area.
+// Also parses the tags, radius, min, max to be used for the function run later.
 function locationFinder(location, tags, radius, min, max) {
   var apiURL = `https://maps.googleapis.com/maps/api/geocode/json?&address=${location}&key=${keyAPI}`;
   console.log(apiURL);
   fetch(apiURL)
-      .then(response => response.json())
-      .then(data => {
-
-        if (data.status !== "OK") {
-          invalidPara.innerHTML = `The error message ${data.status} has occured, please enter a valid location/cusine and try again`;
-            return;
-        } else {
-            const placeObj = data.results[0]
-            const location = data.results[0].geometry.location
-            console.log(placeObj)
-            locationMarker(placeObj)
-            map.panTo(location);
-            nearbyPlaces(location, tags, radius, min, max)
-        }})
+    .then(response => response.json())
+    .then(data => {
+      // Status check. Returns if not "OK"
+      if (data.status !== "OK") {
+        invalidPara.innerHTML = `The error message ${data.status} has occured, please enter a valid location/cusine and try again`;
+        return;
+      } else {
+        // Added current place where user has searched and added it to local storage
+        recentResults.push(location);
+        // Stores updated results in local storage
+        localStorage.setItem('recentResults', JSON.stringify(recentResults))
+        const placeObj = data.results[0]
+        const placeLocation = data.results[0].geometry.location
+        // Runs marker command.
+        locationMarker(placeObj)
+        // Pan's to location 
+        map.panTo(placeLocation);
+        // Runs the nearbyPlaces func
+        nearbyPlaces(placeLocation, tags, radius, min, max)
+      }})
 }
 
+// Takes the lat/lng, tags, radius, min and max. Queries these values to get a filtered list of nearby locations that match.
 function nearbyPlaces(input, tags, radius, min, max) {
+  // Refreshes the list.
+  renderList();
+
   const lat = input.lat;
   const lng = input.lng;
-  console.log(min)
-  console.log(max)
 
   var apiURL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${input.lat}%2C${input.lng}&maxprice=${max}&minprice=${min}&radius=${radius}&keyword=${tags}&key=${keyAPI}`;
   console.log(apiURL);
@@ -86,6 +106,7 @@ function nearbyPlaces(input, tags, radius, min, max) {
     });
 }
 
+// Completes a fetch for each value from array parsed into it, will not return until it is complete as it is a promise.
 async function fetchDetails(arrayID) {
 
   const requests = [];
@@ -118,96 +139,129 @@ async function fetchDetails(arrayID) {
 
 const resultsElement = document.getElementById("results");
 
+// Renders the results on screen.
 async function renderData(locationObject) {
   // Clears previous cards if there are any.
   resultsElement.innerHTML = "";
-
-  console.log(locationObject);
-
+  // Empty array for fetchDetails
   const arrayID = [];
-
+  // Only displays 9 or fewer results.
   const slicedResults = locationObject.results.slice(0, 9);
-
+  // Pushes the IDs into the array, for the fetchDetils async func.
   for (let i = 0; i < slicedResults.length; i++) {
     let placeID = slicedResults[i].place_id;
     arrayID.push(placeID);
   }
+  // A small function to display the rating in stars.
+  function renderRating(rate) {
+    let star = "&#9733 "
 
-    function renderRating(rate) {
-
-      let star = "&#9733 "
-
-      for (let i = 0; i <= 5; i++) {
-        if (i === rate) {
-          star = star.repeat(i)
-          return star;
-        }
+    for (let i = 0; i <= 5; i++) {
+      if (i === rate) {
+        star = star.repeat(i)
+        return star;
       }
     }
+  }
+  // A small function to display the price in pound signs.
+  function renderPrice(price) {
+    let pound = "&pound "
 
-    function renderPrice(price) {
-
-      let pound = "&pound "
-
-      for (let i = 0; i <= 4; i++) {
-        if (i === price) {
-          pound = pound.repeat(i)
-          return pound;
-        }
+    for (let i = 0; i <= 4; i++) {
+      if (i === price) {
+        pound = pound.repeat(i)
+        return pound;
       }
     }
+  }
+  // The main bulk of renderDetails func, will render the results on the page.
+  fetchDetails(arrayID)
+    .then(data => {
+    // Local places infomation.
+    const results = data
+    console.log(results)
+    // This allows to get the results and store it within local storage
+    const storedResults = JSON.parse(localStorage.getItem('recentResults')) || [];
+    
+    for (let x = 0; x < results.length; x++) {
+      const resultObj = results[x].result;
+      let imageRef = resultObj.photos[0].photo_reference;
 
-    fetchDetails(arrayID)
-      .then(data => {
-        // Local places infomation.
-        const results = data
-        console.log(results);
+      locationMarker(resultObj);
 
-      const storedResults = JSON.parse(localStorage.getItem('recentResults')) || []; // This allows to get the results and store it within local storage
+      let imgAPI = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=300&photo_reference=${imageRef}&key=${keyAPI}`
 
-      for (let x = 0; x < results.length; x++) {
-        const resultObj = results[x].result;
-        locationMarker(resultObj);
-        const cardContent =
-          `
-          <h3>${resultObj.name}</h3>
-          <div class="result-content">
-            <img class="result-img" src="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${resultObj.photos[0].photo_reference}&key=${keyAPI}">
-            <div class="result-content-info">
-              <a href="https://www.google.com/maps/dir/?api=1&origin=${locationElement.value}&destination=${resultObj.formatted_address}&destination_place_id=${resultObj.place_id}" target="_blank">Directions <i class="fa-solid fa-route"></i></a>
-              <a href="${resultObj.website}" target="_blank">Website <i class="fa-solid fa-globe"></i></a>
-              <p>Price Level: <span class="rating">${(renderPrice(resultObj.price_level) || "N/A")}</span></p>
-              <p>Rating: <span class="rating">${resultObj.rating || "N/A"}</span></p>
-              <div class="result-review">
-                <p>${resultObj.reviews[0].text}</p>
-                <i>- ${resultObj.reviews[0].author_name} ${(renderRating(resultObj.reviews[0].rating))}</i> 
-              </div>
-              </div>
-          </div>
-          `
-
-          storedResults.push(resultObj); // Added current place where user has searched and added it to local storage
-
-          let newResult = document.createElement('article');
-          newResult.classList.add('result-card');
-          newResult.innerHTML = cardContent;
-
-          // To add the results and display it with text
-          var resultList = document.querySelector(".result-list");
-          var listElement = document.createElement('li');
-
-          listElement.textContent = place.name;
-          resultList.appendChild(listElement)
-          
-
-
-          resultsElement.appendChild(newResult);
+      if (!imageRef) {
+        let imgAPI = `https://placehold.co/300x300`
       }
 
-      localStorage.setItem('recentResults', JSON.stringify(storedResults)); // Stores updated results in local storage
-    });
+      const cardContent =
+        `
+        <h3>${resultObj.name}</h3>
+        <div class="result-content">
+          <img class="result-img" src="${imgAPI}">
+          <div class="result-content-info">
+            <a href="https://www.google.com/maps/dir/?api=1&origin=${locationElement.value}&destination=${resultObj.formatted_address}&destination_place_id=${resultObj.place_id}" target="_blank">Directions <i class="fa-solid fa-route"></i></a>
+            <a href="${resultObj.website}" target="_blank">Website <i class="fa-solid fa-globe"></i></a>
+            <p>Price Level: <span class="rating">${(renderPrice(resultObj.price_level) || "N/A")}</span></p>
+            <p>Rating: <span class="rating">${resultObj.rating || "N/A"}</span></p>
+            <div class="result-review">
+              <p>${resultObj.reviews[0].text}</p>
+              <i>- ${resultObj.reviews[0].author_name} ${(renderRating(resultObj.reviews[0].rating))}</i> 
+            </div>
+            </div>
+        </div>
+        `
+      console.log(cardContent)
+      let newResult = document.createElement('article');
+      newResult.classList.add('result-card');
+      newResult.innerHTML = cardContent;
+      resultsElement.appendChild(newResult)
+    }
+
+
+  });
 }
 
+// Loads the recentsearches from Local Storage.
+function loadRecentSearches() {
+  const storedResults = JSON.parse(localStorage.getItem('recentResults'));
+
+  // Checks to see if there is any localstorage.
+  if (storedResults) {
+    recentResults = storedResults;
+  } else {
+    return;
+  }
+
+  renderList();
+
+}
+
+//Query Selector for result list
+const resultList = document.querySelector(".result-list");
+
+// Renders the list in HTML
+function renderList() {
+  // Clears the list.
+  resultList.innerHTML =""
+
+  // Gets the latest result from the array.
+  let recentValue = recentResults.splice(-1)
+
+  // To add the results and display it with text
+  const listElement = document.createElement('li');
+  const name = recentValue[0]
+
+  listElement.textContent = name;
+  listElement.classList.add('button')
+  resultList.appendChild(listElement)
+
+  listElement.addEventListener('click', () => {
+    console.log("Test")
+  })
+
+}
 
 // Query Selectors
 const locationElement = document.querySelector("#location");
@@ -263,3 +317,5 @@ function locationSearch(event) {
     locationFinder(location, tags, radius, minPrice, maxPrice);
   }
 }
+
+loadRecentSearches();
